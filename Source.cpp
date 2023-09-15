a519dbbe135005c6f074ae158996f1230fb75233
matts <matts@MATTILION>
2023-09-15 16:31:22 -0700

    Added some comments explaining macros

--- Source.cpp
+++ Source.cpp
@@ -3,20 +3,23 @@
 #include <utility>
 #include <vector>
 
+/* Deconstruction of Vulkan's VkShaderModule object*/
 #define TEST_HANDLE(object) typedef struct object##_T *object;
 TEST_HANDLE(foo);
 
-foo adrs;
-foo* memaddress = &adrs;
+/* I assume that Vulkan creates a pointer on the GPU device to the object, but idk */
+foo deviceShaderModule;
+foo* deviceAddress = &deviceShaderModule;
 
+/* Test functions to mimick the Vulkan equivalents */
 void create(foo* pFoo) {
-    *memaddress = *pFoo;
+    *deviceAddress = *pFoo;
     std::cout << "Foo address is: " << pFoo << std::endl;
 }
 void destroy(foo Foo) {
     std::cout << "I am destroying Foo!" << std::endl;
-    if (memaddress == &Foo) {
-        delete[] memaddress;
+    if (deviceAddress == &Foo) {
+        delete[] deviceAddress;
         delete[] Foo;
     }
 }
@@ -53,12 +56,14 @@ struct rofTest {// Desconstructed version of my shader object
 
 int main()
 {
-    std::cout << "memaddress is: " << memaddress << std::endl;
+    std::cout << "The device address is: " << deviceAddress << std::endl;
     try {
+
         std::vector<rofTest> testi
         {
             {"file1.shader", test1}, {"file2.shader", test2}, {"file3.shader", test3}
         };
+
     }
     catch (const std::exception& e) {
         std::cout << e.what() << std::endl;
